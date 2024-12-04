62e6ecf18473eab5a584c7512b46433c6e74adaf
Alexandre Desoutter <alexd@MacBook-Pro-de-Alexandre.local>
2024-12-04 09:17:20 +0100

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Hello, World2!");
         //Commentaire ici
         //🗿
-        //Un truc en dessous
+        System.out.println("Hello, World2!");
     }
 }
